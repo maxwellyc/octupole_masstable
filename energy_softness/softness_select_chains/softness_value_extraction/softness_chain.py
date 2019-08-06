16fef547976fe9177bbc9f2b1be4764f878b6068
Maxwell Cao (Yuchen) <maxwellyuchen@gmail.com>
2019-08-06 14:42:29 -0400

    Ba chain softness calculation update

---
+++ energy_softness/softness_select_chains/softness_value_extraction/softness_chain.py
@@ -0,0 +1,14 @@
+# read-in softness base, there'll be two parts
+# baseline can be achieved for octupole deformed and non-oct nuclei with:
+# 1. octupole nuclei's quad. minimum has been calculated in the octupole energy
+# series calculations.
+# 2. non-oct quad. are the recently calculated ones that are used to initialize
+# softness calculations.
+
+# need to make sure beta2 are exactly the same between the two subtracted states
+# so we'll need two columns to compare before and after beta2 values.
+
+# We'll need 3 input files. beta3 = 0.01 file; baseline file for oct and non-oct.
+# Also we need to find out non-converged states and see what can be done there.
+
+# Let's first gather the latter two files while waiting for beta3 = 0.01 calculations
